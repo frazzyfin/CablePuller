f7c5c70b60ac4f924c5b28d2f326ee1660e3381b
Fraser Finlay <fraserfinlay@gmail.com>
2017-06-03 17:13:06 +0100

    Added working PDF converter code with example PDF

--- CablePuller/MainWindow.xaml.cs
+++ CablePuller/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using ImageMagick;
 
 namespace CablePuller
 {
@@ -23,6 +26,27 @@ namespace CablePuller
         public MainWindow()
         {
             InitializeComponent();
+            Directory.CreateDirectory("ConvertedImages");
+
+            MagickReadSettings settings = new MagickReadSettings();
+            // Settings the density to 300 dpi will create an image with a better quality
+            settings.Density = new Density(300, 300);
+
+            using (MagickImageCollection images = new MagickImageCollection())
+            {
+                // Add all the pages of the pdf file to the collection
+                images.Read("Example.pdf", settings);
+
+                int page = 1;
+                foreach (MagickImage image in images)
+                {
+                    // Write page to file that contains the page number
+                    Trace.Write("Converting page " + page + "...");
+                    image.Write(@"ConvertedImages\Example.Page" + page + ".png");
+                    Trace.WriteLine("done");
+                    page++;
+                }
+            }
         }
     }
 }
